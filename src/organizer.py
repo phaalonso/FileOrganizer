d981545748847e697b02548962ec5a5cd3d3688b
phaalonso <phaalonso@gmail.com>
2020-06-17 00:05:44 -0300

    Great changes in size processing, removing some desnecessary process and better printing in verbose argument

--- src/organizer.py
+++ src/organizer.py
@@ -4,7 +4,7 @@ import sys
 import argparse
 import json
 
-def parse_size(size):
+def parse_size(size) -> int:
     if size.lower().endswith('g'):
         parsed = int(size.lower().split('g')[0]) * 1000
     elif size.lower().endswith('m'):
@@ -13,30 +13,11 @@ def parse_size(size):
         parsed = int(size)
     return parsed
 
-def process_size(minSize, maxSize):
-    # The args parser receives a str, or use the default value that is a number
-    if isinstance(minSize, str):
-        min_size = parse_size(minSize)
-    else:
-        min_size = minSize
-
-    # The args parser receives a str, or use the default value that is a number
-    if isinstance(maxSize, str):
-        max_size = parse_size(maxSize)
-    else:
-        max_size = maxSize
-
-    # My eyes '-'
-    return min_size, max_size
-
-def move_files(file, min_size, max_size):
+def move_files(path, file, min_size, max_size):
     ''' 
         This function will move the file if its size is above the minimun and below the maximun size
     '''
 
-    # File path
-    path = os.path.realpath(args.path)
-    
     file_path = os.path.join(path, file)
     
     # If its a dir
@@ -51,15 +32,12 @@ def move_files(file, min_size, max_size):
 
     file_type = file.split('.')[-1]
 
+    # If the file_size don't respect the constraints or it's in the ignore list then return
+    if max_size > file_size < min_size or file_type in ignore:
+        return
+
     # Get custom name, otherwise dir_name will have file_type
     dir_name = custom_dir.get(file_type, file_type)
-
-    if max_size > file_size < min_size:
-        return
-
-    if file_type in ignore:
-        return
-
 
     # Path were the file will be moved
     dir_path = os.path.join(path, dir_name)
@@ -89,7 +67,9 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--custom', help='Custom name to store a file type', action='store', nargs=2)
 
     args = parser.parse_args()
-    print(args)
+    
+    if args.verbose:
+        print(args)
 
     ignore = []
     custom_dir = {}
@@ -106,17 +86,28 @@ if __name__ == '__main__':
         # print(f'File type: {file_type}, custom name: {custom_name}')
         custom_dir[file_type] = custom_name 
 
-    print(custom_dir)
+    if args.verbose:
+        print(f'Custom dir: ${custom_dir}')
     
     # Process the sizes of the files
     if args.ignore:
         ignore.append(args.ignore)
     # print(ignore)
 
-    min_size, max_size = process_size(args.minSize, args.maxSize)
+    min_size = parse_size(args.minSize) if isinstance(args.minSize, str) else args.minSize
+    max_size = parse_size(args.maxSize) if isinstance(args.maxSize, str) else args.maxSize
 
-    # Get the files list
-    files_list: list = os.listdir(args.path)
+    if args.verbose:
+        print(f'Minimun size: ${min_size}\nMaximun size: ${max_size}')
+
+    try: 
+        # Get the files list
+        pathToDir = os.path.realpath(args.path)
+        files_list: list = os.listdir(pathToDir)
+    except FileNotFoundError:
+        print('Diretório não encontrado!')
+        exit()
+
     if files_list.__len__() == 0:
         print(f'The path {args.path} is empty!')
     else:
@@ -124,4 +115,4 @@ if __name__ == '__main__':
             print(files_list)
 
         for file in files_list:
-            move_files(file, min_size, max_size)
+            move_files(pathToDir, file, min_size, max_size)
